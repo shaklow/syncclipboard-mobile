42ec0a10c1001d4a417e1c5239d3f46cc17c59cc
Erenche <erenche@qq.com>
2026-07-10 09:54:43 +0800

    chore: bump version to 1.0.0-alpha9 (versionCode 9)

--- clipboard-xposed/build.gradle.kts
+++ clipboard-xposed/build.gradle.kts
@@ -9,8 +9,8 @@ plugins {
 }
 
 extra["appPackageName"] = "io.github.erenche.syncclipboard"
-extra["appVersionCode"] = 8
-extra["appVersionName"] = "1.0.0-alpha8"
+extra["appVersionCode"] = 9
+extra["appVersionName"] = "1.0.0-alpha9"
 extra["compileSdkVersion"] = 37
 extra["targetSdkVersion"] = 37
 extra["minSdkVersion"] = 29
